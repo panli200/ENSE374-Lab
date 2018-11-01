61233642f00c23f01bd0e30680a914b7d4b9af66
panli200 <44111434+panli200@users.noreply.github.com>
2018-10-31 18:46:46 -0600

    uploading the files

--- Ense374_lab3/src/lab3/Employee.java
+++ Ense374_lab3/src/lab3/Employee.java
@@ -1,5 +1,72 @@
 package lab3;
 
 public class Employee {
+	 private String fNam;
+	    private String lN;
+	    private String addr;
+	    private String NUM;
+	    private String hireDate;
+	    private String dol;
+
+	    public Employee (){}
+
+	    public void setSTUFFUP(String fNam, String lN, String addr, String NUM,
+	    String hireDate, String dol)
+	    {
+	        this.fNam = fNam;
+	        this.lN = lN;
+	        this.addr = addr;
+	        this.NUM = NUM;
+	        this.hireDate = hireDate;
+	        this.dol = dol;
+	    }
+
+	    public String getfNam()
+	    {
+	        return this.fNam;
+	    }
+
+	    public void setfNam(String fNam)
+	    {
+	        this.fNam = fNam;
+	    }
+
+	    public String getlN()
+	    {
+	        return this.lN;
+	    }
+
+	    public String getaddr()
+	    {
+	        return this.addr;
+	    }
+
+	    public String getNUM()
+	    {
+	        return this.NUM;
+	    }
+
+	    public String getHireDate()
+	    {
+	        return this.hireDate;
+	    }
+
+	    public String getdol()
+	    {
+	        return this.dol;
+	    }
+
+	    public void printEmployeeData()
+	    {
+	        System.out.format(
+	            "Name: %s %s| addr: %s| Phone Number: %s|" +
+	            " Hire Date: %s| dol: %s \n",
+	            this.fNam,
+	            this.lN,
+	            this.addr,
+	            this.NUM,
+	            this.hireDate,
+	            this.dol);
+	    }
 
 }
--- Ense374_lab3/src/lab3/EmployeeApplication.java
+++ Ense374_lab3/src/lab3/EmployeeApplication.java
@@ -1,5 +1,76 @@
 package lab3;
 
 public class EmployeeApplication {
+	//test
+
+	public static void main(String[] args)
+    {
+    	Employee[] eD = new Employee[3];
+		int eDSize = 0;
+		EmployeeApplication eA = new EmployeeApplication();
+		eA.addEs(eD);
+		eA.prints(eD);
+		Employee[] sorted = new Employee[3];
+		sorted = eA.sorteDByName(eD,3);
+		eA.prints(sorted);
+
+    }
+
+
+	public Employee[] sorteDByName(Employee[] database, int databaseSize)
+    {
+        Employee[] srtD = database;
+        for(int i=1; i<(databaseSize); i++)
+        {
+            for(int j=0; j<(databaseSize-i); j++)
+            {
+                if (srtD[j].getlN().compareTo(srtD[j+1].getlN())>0)
+                {
+                    Employee temp = srtD[j+1];
+                    srtD[j+1] = srtD[j];
+                    srtD[j] = temp;
+                }
+            }
+        }
+        return srtD;
+    }
+
+	public void addEs(Employee[] eD)
+	{
+		Scanner input = new Scanner(System.in);
+
+		for (int i = 0; i < 3; ++i)
+		{
+			eD[i] = new Employee();
+			System.out.print("Enter an Employee's First Name: ");
+			String fName = input.nextLine();
+			System.out.print("Enter an Employee's Last  Name: ");
+			String lName = input.nextLine();
+			System.out.print("Enter an Employee's Address: ");
+			String addr = input.nextLine();
+			System.out.print("Enter an Employee's Phone Number: ");
+			String NUM = input.nextLine();
+			System.out.print("Enter an Employee's Hire Date: ");
+			String hireDate = input.nextLine();
+			System.out.print("Enter an Employee's starting Salary: ");
+			String dol = input.nextLine();
+
+			eD[i].setSTUFFUP(fName, lName, addr,  NUM, hireDate,  dol);
+
+
+
+		}
+	}
+
+	public void prints(Employee[] eD)
+	{
+		for (int i = 0; i < 3; ++i)
+		{
+
+			eD[i].printEmployeeData();
+
+		}
+
+	}
 
 }
